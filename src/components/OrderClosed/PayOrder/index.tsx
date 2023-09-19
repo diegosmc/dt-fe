7d72afd852028a579ca4ad3b6e4e96f1633d91de
Diego Cardoso <diego.cardoso@nave.rs>
2023-09-19 10:10:53 -0300

    Alterado comportamento de botão

--- src/components/OrderClosed/PayOrder/index.tsx
+++ src/components/OrderClosed/PayOrder/index.tsx
@@ -1,33 +1,22 @@
 import { useCart } from '../../../hooks/useCart';
+
 import { currencyFormats } from '../../../helpers/currencyFormats';
+
 import { Container } from '../style';
+
 import { useState } from 'react';
 
 export function PayOrder() {
   const { cart } = useCart();
+  
   const totalAmount = cart.reduce((acc, item) => (acc += Number(item.subtotal)), 0);
-  const [payOrder, setPayOrder] = useState(false);
-
-  const showAlert = () => {
-    setPayOrder(true);
-
-    /* Resetar o estado após um determinado tempo (opcional)
-    setTimeout(() => {
-      setPayOrder(false);
-    });*/
-  };
 
   return (
     <Container>
-      <button type='button' onClick={showAlert}>
-        Finalizar Pedido
-      </button>
+      <button type='submit'>Finalizar Pedido</button>
       <span>
-        {payOrder ? (
-          <strong className="fade-in">Pedido Concluído</strong>
-        ) : (
-          <strong>{currencyFormats(totalAmount)}</strong>
-        )}
+        Pedido Concluido <strong>{currencyFormats(totalAmount)}</strong>
+        
       </span>
     </Container>
   );
